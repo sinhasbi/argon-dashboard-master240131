5a92d781c9d368cb2ee253817b0d90dd91aa0c21
Hasbi <sinhasbi123@gmail.com>
2024-01-31 16:24:07 +0800

    新增PRODUCTSPAGES

--- pages/tables.php
+++ pages/tables.php
@@ -108,7 +108,7 @@ if (isset($_GET["search"])) {
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="./pages/tables.html">
+          <a class="nav-link active" href="../pages/tables.php">
             <div
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
@@ -117,7 +117,7 @@ if (isset($_GET["search"])) {
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="./pages/billing.html">
+          <a class="nav-link " href="../pages/product-list.php">
             <div
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
